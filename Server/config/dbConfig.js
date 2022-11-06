c15d62fcd71f451d6e342d1547a37a4ef65abf74
Adomcikas <adomasgrauzelis@gmail.com>
2022-11-06 08:05:00 -0900

    db

--- Server/config/dbConfig.js
+++ Server/config/dbConfig.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: 'ltnya0pnki2ck9w8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
     USER: 'b3m1i7l85gmnmp2f',
     PASSWORD: 'l81z1bn5m10l4n9t',
-    DB: 'Rental_Management_db',
+    DB: 'qm9lkmsirtnjs7p4',
     dialect: 'mysql',
 
     pool: {
